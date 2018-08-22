799eb045b43763bc04bea1124f4751b0a4d51eab
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-08-22 20:09:17 +0200

    named pipe added

--- ipc_poc/Program.cs
+++ ipc_poc/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.IO.Pipes;
 
 namespace ipc_poc
 {
     class Program
     {
+        public const string NameOfPipe = "ipc_poc_pipe";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Server starting...");
+            var pipe = new NamedPipeServerStream(NameOfPipe, PipeDirection.InOut);
+            
+            Console.WriteLine("Waiting for connection...");
+            pipe.WaitForConnection();
+            Console.WriteLine("Connected!");
+            
+            using (var writer = new StreamWriter(pipe))
+            {
+                writer.AutoFlush = true;
+                pipe.WaitForPipeDrain();
+                var message = "";
+                
+                while (!message.Contains(":q"))
+                {
+                    Console.Write("Enter message to send to client (enter \":q\" to exit): ");
+                    message = Console.ReadLine();
+                    writer.WriteLine(message);    
+                }
+            }
+            
+            pipe.Close();
+            Console.WriteLine("Server closed...");
         }
     }
 }
