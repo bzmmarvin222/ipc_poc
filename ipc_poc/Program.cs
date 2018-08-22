dde68796ea1fd298909cf6acacb32f39821ec156
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-08-22 18:15:21 +0200

    init

---
+++ ipc_poc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ipc_poc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
